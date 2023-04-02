e191d794a054e38b6959d3ac4ce5741d352a8f2d
sahandilshan <sahandilshan222@gmail.com>
2023-04-02 21:54:33 +0530

    Modify the logic to connect to the DB

--- src/main/java/com/wso2/custom/risk/auth/function/dao/UserProfileDaoImp.java
+++ src/main/java/com/wso2/custom/risk/auth/function/dao/UserProfileDaoImp.java
@@ -1,6 +1,5 @@
 package com.wso2.custom.risk.auth.function.dao;
 
-import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -10,7 +9,6 @@ import com.wso2.custom.risk.auth.function.util.ConfigReader;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
-import org.wso2.carbon.identity.core.persistence.JDBCPersistenceManager;
 
 import java.util.Optional;
 
@@ -21,15 +19,12 @@ public class UserProfileDaoImp implements UserProfileDao {
     private MongoDatabase riskAuthDatabase;
     private MongoCollection<Document> userAttributeCollection;
 
-    private final static String MONGO_DB_URI = "mongodb://localhost:27017";
     private final static String DATABASE_NAME = "WSO2_IS_RISK_AUTH";
     private final static String COLLECTION_NAME = "USER_ATTR";
 
 
 
     private UserProfileDaoImp () {
-
-
 
     }
 
@@ -87,13 +82,12 @@ public class UserProfileDaoImp implements UserProfileDao {
 
         Optional<String> dbUriOptional = ConfigReader.getDBUri();
         if (dbUriOptional.isPresent()) {
-            this.mongoClient = MongoClients.create(MONGO_DB_URI);
+            this.mongoClient = MongoClients.create(dbUriOptional.get());
         } else {
             throw new UnsupportedOperationException("URI is required to create the connection with the MongoDB.");
             // TODO add the support to just create the mongo connection with host, port, username & password
             //  (without the mongoURI).
         }
-        this.mongoClient = MongoClients.create(MONGO_DB_URI);
         this.riskAuthDatabase = mongoClient.getDatabase(DATABASE_NAME);
         this.userAttributeCollection = riskAuthDatabase.getCollection(COLLECTION_NAME);
     }
